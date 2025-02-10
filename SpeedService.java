85208c114712e18bd2f1c0574245200d2c1ebacd
harishy40 <harish.y40@gmail.com>
2025-02-10 23:30:31 +0530

    UpdateSpeedService
    UpdateSpeedService

--- SpeedService.java
+++ SpeedService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 public class SpeedService {
-    private final Map<String, RentalData> activeRentals = new HashMap<>();
+    private final Map<String, RentalData> rentals = new HashMap<>();
     private final List<NotifyInfService> notificationServices;
 
     public SpeedService(List<NotifyInfService> notificationServices) {
@@ -13,23 +13,19 @@ public class SpeedService {
     }
 
     public void registerRental(RentalData rental) {
-        activeRentals.put(rental.getCarId(), rental);
+        rentals.put(rental.getCarId(), rental);
     }
 
     public void processSpeedData(SpeedData event) {
-        if (!activeRentals.containsKey(event.getCarId())) {
-            System.out.println(" No rental found for car: " + event.getCarId());
-            return;
-        }
 
-        RentalData rental = activeRentals.get(event.getCarId());
+        RentalData rental = rentals.get(event.getCarId());
         if (event.getSpeed() > rental.getMaxSpeedLimit()) {
-            String warningMessage = " Car crossd speed limit" + rental.getCarId() +
-                                    " exceeded " + rental.getMaxSpeedLimit() + " km/h.";
+            String warning = " Car crossd speed limit" + rental.getCarId() +
+                                    " exceeded " + rental.getMaxSpeedLimit();
             
             for (NotifyInfService service : notificationServices) {
-                service.notifyRenter(rental.getRenterId(), warningMessage);
-                service.notifyRentalCompany(rental.getRentalId(), warningMessage);
+                service.notifyRenter(rental.getRenterId(), warning);
+                service.notifyRentalCompany(rental.getRentalId(), warning);
             }
         }
     }
